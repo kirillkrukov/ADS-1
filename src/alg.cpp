8e06091d27dbf759733a425f91df29636b639f82
kirillkrukov <78787733+kirillkrukov@users.noreply.github.com>
2021-04-01 16:19:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,9 +8,9 @@ int cbinsearch(int* arr, int size, int value) {
         mid = (left + right) / 2;
         if (arr[mid] > value)
             right = mid;
-        if (arr[mid] < value) {
+        if (arr[mid] < value)
             left = mid + 1;
-        } else {
+        if (arr[mid] == value) {
             count = 1;
             break;
         }
