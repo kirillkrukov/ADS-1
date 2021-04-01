a800bf1f89acc3ac3a94aaf6109e6c0ca3264c30
kirillkrukov <78787733+kirillkrukov@users.noreply.github.com>
2021-04-01 16:07:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,41 @@
-// Copyright 2021 NNTU-CS
-
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    int left = 0;
+    int right = size - 1;
+    int count = 0;
+    int c = 1;
+    int mid = 0;
+    while (left < right) {
+        mid = (left + right) / 2;
+        if (arr[mid] > value)
+            right = mid;
+        if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            count = 1;
+            break;
+        }
+    }
+    int n = 0;
+    while (n == 0) {
+        if (arr[mid + c] == value) {
+            count++;
+            c++;
+        } else {
+            n = 1;
+        }
+    }
+    n = 0;
+    c = 1;
+    while (n == 0) {
+        if (arr[mid - c] == value) {
+            count++;
+            c++;
+        } else {
+            n = 1;
+        }
+    }
+    if (count == 0)
+        return 0;
+    else
+        return count;
 }
